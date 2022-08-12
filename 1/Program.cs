7acdd8f061925f0599277788e01744eff161d556
Vitaliy Novozhilov <don@vitalino.ru>
2022-08-13 00:10:27 +0300

    Home work 1

---
+++ 1/Program.cs
@@ -0,0 +1,23 @@
+﻿int num; 
+
+while (true)
+{
+      Console.Write("Введите трёхзначное число: ");
+      if (int.TryParse(Console.ReadLine(), out num) && num < 1000 && num > 100 || num > -1000 && num < -100 )
+           
+            break;
+      Console.WriteLine("Ошибка ввода! Введите трёхзначное число!");
+}
+
+Console.WriteLine($"Введено число = {num}");
+
+num = num % 100;
+num = num/10;
+
+Console.WriteLine($"Вторая цифра числа = {num}");
+
+
+
+
+
+
