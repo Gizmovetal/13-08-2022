6c18481e3210f6637d1c9046d1c4dbf4bdc5a3e6
Vitaliy Novozhilov <don@vitalino.ru>
2022-08-13 00:30:43 +0300

    Home work 2

---
+++ 2/Program.cs
@@ -0,0 +1,23 @@
+﻿int num; 
+
+while (true)
+{
+    Console.Write("Введите число: ");
+    if (int.TryParse(Console.ReadLine(), out num))
+           
+    break;
+    Console.WriteLine("Ошибка ввода! Введите число!");
+}
+
+if (num < 100 && num > -100) 
+{
+    Console.WriteLine($"Введено число = {num}, третья цифра отсутствует!");        
+}
+else    
+{
+    Console.WriteLine($"Введено число = {num}");
+    Console.WriteLine($"Третья цифра числа = {num.ToString()[2]}");
+}
+
+
+
