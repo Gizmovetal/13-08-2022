d3832013fe246202a5d1085a0f3fc35427faf044
Vitaliy Novozhilov <don@vitalino.ru>
2022-08-13 00:40:06 +0300

    Home work 3

---
+++ 3/Program.cs
@@ -0,0 +1,19 @@
+﻿int num; 
+
+while (true)
+{
+    Console.Write("Введите день недели, число от 1 до 7: ");
+    if (int.TryParse(Console.ReadLine(), out num) && num > 0 && num < 8)
+           
+    break;
+    Console.WriteLine("Ошибка ввода! Введите число от 1 до 7!");
+}
+
+if (num > 5 ) 
+{
+    Console.WriteLine($"Введено число = {num} -> выходной");        
+}
+else    
+{
+    Console.WriteLine($"Введено число = {num} -> будний день");
+}
